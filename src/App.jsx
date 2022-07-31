134531ac1016f82e1d059585e9dad570bb5f0491
dod <maulinaro@yahoo.com>
2022-07-31 21:54:08 +0700

    archive component complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AddNote from "./components/AddNote";
+import Archive from "./components/Archive";
 import Header from "./components/Header";
 import NoteList from "./components/NoteList";
 import { getInitialData } from "./utils/index";
@@ -11,6 +12,7 @@ const App = () => {
       <main className="note-app__body">
         <AddNote />
         <NoteList notes={getInitialData} />
+        <Archive notes={getInitialData} />
       </main>
     </>
   );
